71aed1fdf2a3c2714ea77f6fc664d4380b181310
Singuru Abhiram <62535575+Abhi-hue5123@users.noreply.github.com>
2020-08-07 13:02:42 +0530

    made coupons and name copumn not necessary

--- MDA/MDA/urls.py
+++ MDA/MDA/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v.index, name="index"),
+    path('index.html', v.index, name="index"),
     path('accounts/',include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('adduser/',v.simple_upload,name='simple_upload'),
--- MDA/socius/models.py
+++ MDA/socius/models.py
@@ -10,6 +10,6 @@ class Destination(models.Model):
     offer = models.BooleanField(default=False)
 
 class UserList(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(max_length=254,blank=True)
-    coupon = models.CharField(max_length=100)
+    coupon = models.CharField(max_length=100, blank=True, null=True)
